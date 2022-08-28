c660dbcbf913db8e4e9a514d2ee4fcd63dc82668
Kieran Singh <kieransingh27@gmail.com>
2022-08-28 15:01:01 +1000

    Added submit container to comment create form as well as state to toggle on and off

--- src/Components/Widgets/createComment.jsx
+++ src/Components/Widgets/createComment.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 const CreateComment = () => {
   const [commentContent, setCommentContent] = useState("");
+  const [showSubmit, setShowSubmit] = useState(false);
 
   const handleChange = (e) => {
     setCommentContent(e.target.value);
@@ -12,6 +13,14 @@ const CreateComment = () => {
     e.preventDefault();
   };
 
+  const handleFocus = () => {
+    setShowSubmit((showSubmit) => !showSubmit);
+  };
+
+  const handleClick = () => {
+    setShowSubmit((showSubmit) => !showSubmit);
+  };
+
   return (
     <div className={styles.createCommentContainer}>
       <div className="avatar-placeholder"></div>
@@ -19,14 +28,24 @@ const CreateComment = () => {
         <form onSubmit={handleSubmit}>
           <div>
             <textarea
-              cols="30"
+              cols="15"
               rows="10"
               required
               placeholder="Add to the discussion..."
               value={commentContent}
               onChange={handleChange}
+              onFocus={handleFocus}
             ></textarea>
           </div>
+          <span></span>
+          {showSubmit ? (
+            <div className={styles.createCommentFormFooter}>
+              <button onClick={handleClick}>Cancel</button>
+              <input type="submit" value="Submit" />
+            </div>
+          ) : (
+            ""
+          )}
         </form>
       </div>
     </div>
