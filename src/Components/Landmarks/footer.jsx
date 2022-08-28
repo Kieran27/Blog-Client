cb465e34ae96ed0472c7837f52129208d8ffaffa
Kieran Singh <kieransingh27@gmail.com>
2022-08-28 13:24:52 +1000

    Added footer information about project

--- src/Components/Landmarks/footer.jsx
+++ src/Components/Landmarks/footer.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import styles from "./landmarks.module.scss";
 
 const Footer = () => {
-  return <footer>Made By Kieran Singh</footer>;
+  return (
+    <footer>
+      <div className={styles.footerContainer}>
+        <p>
+          <strong>Bloggr</strong> - A functional, fullstack blogging site
+        </p>
+        <p>
+          Built with the <strong>MERN</strong> stack and hard work
+        </p>
+        <p>
+          Developed by <strong>Kieran Singh</strong>
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
